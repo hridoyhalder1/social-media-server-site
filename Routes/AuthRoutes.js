5b07905a145f5421204f10d2cd80c80d04c171a2
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-12-31 23:52:31 +0600

    authentication setup

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser } from "../Controllers/AuthController.js";
+import { loginUser, registerUser } from "../Controllers/AuthController.js";
 
 const router = express.Router()
 
@@ -8,5 +8,6 @@ const router = express.Router()
 // })
 
 router.post('/register', registerUser)
+router.post('/login', loginUser)
 
 export default router
